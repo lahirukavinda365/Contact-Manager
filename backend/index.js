18d901b89e70e611bae1be963fb0d285745098a6
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2023-11-20 15:42:52 +0530

    DB connection completed

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import contactRoutes from './routes/contactRoutes.js'
+import mongoose, { connect } from "mongoose";
 
 dotenv.config();
 
@@ -13,3 +14,15 @@ app.use('/api/contacts', contactRoutes);
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
 })
+
+
+try {
+     const connect = await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB connected...",  connect.connection.host, connect.connection.name);
+} catch (error) {
+    console.log("DB connection failed....");
+    console.log(error);
+}
+
+
+   
